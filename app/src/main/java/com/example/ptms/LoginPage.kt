dfab2e85d435c65bf659859aac50057d0f651163
Tarun Kumar Kushwaha <74929799+kk-tarun@users.noreply.github.com>
2023-04-08 00:37:55 +0530

    updated LoginPage

--- app/src/main/java/com/example/ptms/LoginPage.kt
+++ app/src/main/java/com/example/ptms/LoginPage.kt
@@ -5,29 +5,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginPage : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var username: EditText
     private lateinit var password: EditText
+    private lateinit var register:TextView
     private lateinit var loginButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login_page)
-            username = findViewById(R.id.username)
-            password = findViewById(R.id.password)
-            loginButton = findViewById(R.id.loginButton)
 
-            loginButton.setOnClickListener {
-                if (username.text.toString() == "user" && password.text.toString() == "1234") {
-                    Toast.makeText(this, "Login Successful!", Toast.LENGTH_SHORT).show()
-                    val i:Intent = Intent(applicationContext,FrontPage::class.java)
-                    startActivity(i);
-                } else {
-                    Toast.makeText(this, "Login Failed!", Toast.LENGTH_SHORT).show()
+        setContentView(R.layout.activity_login_page)
+        username = findViewById(R.id.username)
+        password = findViewById(R.id.password)
+        loginButton = findViewById(R.id.loginButton)
+        register = findViewById(R.id.signupText)
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        loginButton.setOnClickListener {
+            val regno= username.text.toString()
+            val pass = password.text.toString()
+            if (regno.isNotEmpty() && pass.isNotEmpty()) {
+                firebaseAuth.signInWithEmailAndPassword(regno, pass).addOnCompleteListener {
+                    if (it.isSuccessful) {
+                        val i: Intent = Intent(applicationContext, FrontPage::class.java)
+                        startActivity(i);
+                    } else {
+                        Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+
+                    }
 
                 }
             }
         }
-
+        register.setOnClickListener {
+            var intent: Intent = Intent(this, register_page::class.java)
+            startActivity(intent)
+        }
     }
+
+
+}
